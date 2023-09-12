30b3a0802b4a1ff5b64a67d5e41f3eb7378627a3
Marc <olmoscodes@gmail.com>
2023-09-12 15:03:13 +0200

    adding olmoscodes.com url to the CORS

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin",
 			"http://localhost:4200");
+	res.header("Access-Control-Allow-Origin",
+		"https://olmoscodes.com");
 	res.header("Access-Control-Allow-Headers",
 			"Origin, X-Requested-With, Content-Type, Accept");
 	next();
